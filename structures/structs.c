3c3a99e1e6efd6c9a930ac0e822d24fd85c4636b
HUMURA ELVIN <humura@humura>
2024-04-30 07:47:34 +0200

    changes made

--- structures/structs.c
+++ structures/structs.c
@@ -17,13 +17,17 @@ struct str2
 
 int main(){
 
-	struct str1 var1 = {1, 'A', 0.976, 'Rwanda coding academy'}, var2;
+	struct str1 var1 = {1, 'A', 0.976, "Rwanda"}, var2;
 
 	struct str2 var3 = {.ff=2.00, .cc='B', .ii=50};
 
 	var1 = var2;
 
-	printf
+	printf("Structure 1: \n\t i = %d, c=%c, f=%f, s=%s \n", var1.i, var1.c, var1.f, var1.s);
+
+	printf("Structure 2: \n\t i = %d, c=%c, f=%f, s=%s \n", var2.i, var2.c, var2.f, var2.s);
+
+	printf("Structure 3: \n\t i = %d, c=%c, f=%f \n", var3.ii, var3.cc, var3.ff);
 
 }
 
