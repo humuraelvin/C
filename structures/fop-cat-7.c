2fd7194ff180896c3bd4561712c6847212c81266
HUMURA ELVIN <humura53@gmail.com>
2024-05-29 09:54:25 +0200

    changes made

---
+++ structures/fop-cat-7.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void keyValue(int, char);
+
+union abc
+{
+    int a;
+    char b;
+}var;
+
+struct charSet
+{
+    int i;
+    int s;
+}cs;
+
+int main(){
+    var.a = 5;
+    printf("Member a= %d, member b = %c, size of abc union = %d \n", var.a, var.b,sizeof(var));
+    keyValue(var.a, var.b);
+    printf("Member s= %d, member g = %c, size of cs structure = %d \n", cs.i, cs.s,sizeof(cs));
+    return 0;
+}
+
+
+
