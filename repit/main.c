18e3d8dc328e94ca264c41eedd820efa6c2d542a
humuraelvin <humura53@gmail.com>
2024-06-15 23:47:38 +0200

    changes made

--- repit/main.c
+++ repit/main.c
@@ -3,13 +3,21 @@
 #include <math.h>
 
 int main(){
+
  int num=1;
+
  int multipleof3 = (num%3==0);
+
  int multipleof5 = (num%5==0);
+
  int multipleof15 = (num%15==0);
+
  while(num<=100){
+
   printf("%d\n", num);
+
  num++;
+ 
   if(num==multipleof3){
         printf("Fizz");
     }
