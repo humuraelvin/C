4f8e2fba28f852de9f6d8a993fbd88e7c80b5b4a
humuraelvin <humura53@gmail.com>
2024-06-19 21:55:28 +0200

    changes made

--- sorting_arrays/bubble_sort.c
+++ sorting_arrays/bubble_sort.c
@@ -21,4 +21,30 @@ void bubbleSort(int arr[], int n){
     
 }
 
-void printArray(int arr){}
+void printArray(int arr[], int size){
+    int i;
+
+    for ( i = 0; i < size; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+
+}
+
+int main(){
+
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    
+    return 0;
+
+}
