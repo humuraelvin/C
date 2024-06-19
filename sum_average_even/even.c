191c330c94bf03f661efca1d739c63a29cab28f7
humuraelvin <humura53@gmail.com>
2024-06-19 20:17:27 +0200

    changes made

---
+++ sum_average_even/even.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(){
+    
+    int n, i ,j, sum;
+
+    printf("To which number do you want to calculate the sum of even numbers");
+    scanf("%d", &n);
+
+    for ( i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+            j+=1;
+        }
+        
+    }
+
+    int average = sum / j;
+
+    printf("SUM = %d", sum);
+    printf("AVERAGE = %d", average);
+    
+    
+
+}
