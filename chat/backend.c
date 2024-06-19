8f13c8608d15229050de9d93e27d3d5e262dc882
humuraelvin <humura53@gmail.com>
2024-06-19 14:41:19 +0200

    changes made

---
+++ chat/backend.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define PORT 8080
+#define BUF_SIZE 1024
+
+void *handle_client(void *arg);
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    pthread_t tid;
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Binding to port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listening for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) >= 0) {
+        printf("New connection accepted\n");
+        pthread_create(&tid, NULL, handle_client, (void *)&new_socket);
+    }
+
+    if (new_socket < 0) {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    close(server_fd);
+    return 0;
+}
+
+void *handle_client(void *arg) {
+    int socket = *(int *)arg;
+    char buffer[BUF_SIZE];
+    int read_size;
+
+    while ((read_size = recv(socket, buffer, BUF_SIZE, 0)) > 0) {
+        buffer[read_size] = '\0';
+        printf("Client: %s\n", buffer);
+        send(socket, buffer, read_size, 0);
+    }
+
+    if (read_size == 0) {
+        printf("Client disconnected\n");
+    } else if (read_size == -1) {
+        perror("recv failed");
+    }
+
+    close(socket);
+    return NULL;
+}
