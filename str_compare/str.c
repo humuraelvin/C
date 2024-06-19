862523577ed61ed4d518c5f97ae095b748ac1c9d
humuraelvin <humura53@gmail.com>
2024-06-19 21:30:17 +0200

    changes made

--- str_compare/str.c
+++ str_compare/str.c
@@ -2,3 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+int main(){
+
+    const char *str1 = "hello";
+    const char *str2 = "hello";
+    const char *str3 = "world";
+    const char *str4 = "hell";
+
+    int result1 = strcmp(str1, str2);
+    int result2 = strcmp(str1, str3);
+    int result3 = strcmp(str1, str4);
+
+
+    printf("Comparing %s and %s gives %d");
+
+}
