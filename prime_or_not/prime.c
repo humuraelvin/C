a5cbb82a472916be48674e55e92aaab9c843d398
humuraelvin <humura53@gmail.com>
2024-06-19 20:27:37 +0200

    changes made

---
+++ prime_or_not/prime.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int lv;
+    int flag = 1;
+
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+
+
+    if (n < 2)
+    {
+        printf("The entered number is not a prime");
+        return 0;
+    }
+
+
+    
+
+;}
