2f0391e72f3a5fac1184e36a8dbcf54965455b2b
humuraelvin <humura53@gmail.com>
2024-06-19 21:34:37 +0200

    changes made

--- str_compare/str.c
+++ str_compare/str.c
@@ -7,9 +7,9 @@ int main() {
     const char *str3 = "world";
     const char *str4 = "hell";
 
-    int result1 = strcmp(str1, str2);  // should return 0
-    int result2 = strcmp(str1, str3);  // should return a negative value
-    int result3 = strcmp(str1, str4);  // should return a positive value
+    int result1 = strcmp(str1, str2);  
+    int result2 = strcmp(str1, str3);  
+    int result3 = strcmp(str1, str4);  
 
     printf("Comparing '%s' and '%s': %d\n", str1, str2, result1);
     printf("Comparing '%s' and '%s': %d\n", str1, str3, result2);
