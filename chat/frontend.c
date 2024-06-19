effcccb20bdd7bd0e6eef829ff7605e28d0a2ca9
humuraelvin <humura53@gmail.com>
2024-06-19 16:44:24 +0200

    changes made

--- chat/frontend.c
+++ chat/frontend.c
@@ -24,7 +24,7 @@ int main() {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
+    
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         printf("Invalid address / Address not supported\n");
         return -1;
