0af7c6300befcd4fafd304343ea5816857eb4528
humuraelvin <humura53@gmail.com>
2024-06-19 21:47:31 +0200

    changes made

--- sorting_arrays/bubble_sort.c
+++ sorting_arrays/bubble_sort.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void bubbleSort(int arr[], int n){
+    int i, j, temp;
+
+    for ( i = 0; i < n-1; i++)
+    {
+        for (j = 0; j < n - i -1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+            
+        }
+        
+    }
+    
+}
