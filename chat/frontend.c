2a184bc1fef84b4ae350867714a0049cc6065d5d
humuraelvin <humura53@gmail.com>
2024-06-19 14:41:31 +0200

    changes made

---
+++ chat/frontend.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+#define BUF_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUF_SIZE] = {0};
+    char message[BUF_SIZE];
+
+    // Creating socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address / Address not supported\n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection failed\n");
+        return -1;
+    }
+
+    printf("Connected to the server. Type messages to send:\n");
+
+    while (1) {
+        printf("You: ");
+        fgets(message, BUF_SIZE, stdin);
+        message[strcspn(message, "\n")] = 0; // Remove newline character
+
+        send(sock, message, strlen(message), 0);
+        int valread = read(sock, buffer, BUF_SIZE);
+        buffer[valread] = '\0';
+        printf("Server: %s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
