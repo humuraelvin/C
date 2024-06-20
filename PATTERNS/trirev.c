bf651724cb90d2e0cdb949f8f2febb9f78ceb7ea
humuraelvin <humura53@gmail.com>
2024-06-20 06:03:29 +0200

    changes made

---
+++ PATTERNS/trirev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int i, j, k, rows =5;
+
+    for ( i = 0; i < rows; i++)
+    {
+        for ( j = 0; i < 2 * i; i++)
+        {
+            printf(" ");
+        }
+
+        for ( k = 0; k < rows - i; k++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+        
+        
+    }
+    
+return 0;
+}
