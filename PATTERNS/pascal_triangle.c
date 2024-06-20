ef729484005dc7e4030d51f513ee382cc089cbe8
humuraelvin <humura53@gmail.com>
2024-06-20 06:59:45 +0200

    changes made

--- PATTERNS/pascal_triangle.c
+++ PATTERNS/pascal_triangle.c
@@ -3,6 +3,33 @@
 
 int main(){
 
+    int height, c, r, pt[10][10];
+
+   
+
+    printf("Enter the height you wish: \n");
+    scanf("%d", &height);
     
+     for ( r = 0; r < height; r++)
+    {
+        for ( c = 0; c < height; c++)
+        {
+            if (c <= r)
+            {
+                if (c == r)
+                {
+                    pt[r][c] = 1;
+                }else
+                {
+                    pt[r][c] = pt[r-1][c] + pt[r-1][c-1];
+                }
+                
+                
+            }
+            printf("\n");
+        }
+        
+    }
+
 
 }
