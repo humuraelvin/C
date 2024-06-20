93e8ed8b7a669eca1ca70b022ec35e501e492d02
humuraelvin <humura53@gmail.com>
2024-06-20 05:45:39 +0200

    changes made

--- PATTERNS/tr.c
+++ PATTERNS/tr.c
@@ -10,11 +10,11 @@ int main(){
 
     for ( i = 0; i <= 5; i++)
     {
-        for ( j = 0; j >= i; j++)
+        for ( j = 5; j >= i; j++)
         {
             printf(" ");
         }
-        for ( k = 0; k <= i; i++)
+        for ( k = 1; k <= i; i++)
         {
             printf("*");
         }
