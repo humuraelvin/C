653727b6851448de7ea02e282a46cde07ead4c0e
humuraelvin <humura53@gmail.com>
2024-06-20 05:44:06 +0200

    changes made

---
+++ PATTERNS/tr.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main(){
+
+    int i, j, k;
+
+
+    for ( i = 0; i <= 5; i++)
+    {
+        for ( j = 0; j >= i; j++)
+        {
+            printf(" ");
+        }
+        for ( k = 0; k <= i; i++)
+        {
+            printf("*");
+        }
+        
+        
+    }
+    
+
+}
